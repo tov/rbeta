5c214ad2ba769968374719e9a0e2cb853456441c
Jesse A. Tov <jesse.tov@gmail.com>
2017-08-01 14:56:26 -0500

    Patterns and bindings

--- pattern.rkt
+++ pattern.rkt
@@ -1,6 +1,53 @@
-#lang racket
+#lang typed/racket
 
-(provide pattern?)
+(provide pattern
+         (rename-out
+           [Pattern? pattern?]))
 
-(define-struct pattern
-  (fields enter exit action))
+(require racket/stxparam)
+(require (for-syntax syntax/parse))
+
+(define-type BindingType (U 'pattern
+                            'object
+                            'reference
+                            'pattern-reference))
+
+(struct Binding ([name   : Symbol]
+                 [type   : BindingType]
+                 [final? : Boolean]
+                 [rhs    : Pattern]))
+
+(struct Pattern ([parent : (U Pattern #false)]
+                 [fields : (Listof Binding)]
+                 [enter  : (Listof Symbol)]
+                 [exit   : (Listof Symbol)]
+                 [action : (-> (-> Void) Void)]))
+
+(define-syntax-parameter inner
+  (λ (stx)
+     (raise-syntax-error #f "use of inner not in an action" stx)))
+
+(define-syntax (pattern stx)
+  (syntax-parse stx
+    [(_ #:super parent:expr
+        ([name:id rhs:expr] ...)
+        #:enter (x:id ...)
+        #:exit (y:id ...)
+        action:expr ...)
+     #'(Pattern parent
+                (list (Binding name 'pattern #true rhs) ...)
+                (list 'x ...)
+                (list 'y ...)
+                (lambda ([inner-f : (-> Void)])
+                  (syntax-parameterize
+                    ([inner (syntax-rules () [(_) (inner-f)])])
+                    action ...
+                    (void))))]))
+
+; A Pattern-object is
+;   (make-pattern-object [Hashtbl-of Symbol Pattern-object]
+;                        [Any ... ->]
+;                        [-> Any ...]
+;                        [->])
+; (define-struct pattern-object
+  ; (fields enter exit action))
